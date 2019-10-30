ee46afc34c88d8d30a3c9aadbc40b6b734bc4745
jnguyen3436 <jnguyen3436@gmail.com>
2019-10-30 14:29:49 -0400

    debugging

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,53 +1,49 @@
-const ctx = document.getElementById('example').getContext('2d');  
+const ctx = document.getElementById("example").getContext('2d');  
 
-// class Game{
-//     constructor(){
-//         this.theHero = new Hero(180, 380, 20,20),
-//         this.obstacleArray = []
-//     }
+    
+  
+class Game{
+    constructor(){
+        this.theHero = new Hero(180, 380, 20,20),
+        this.obstacleArray = []
+    }
 
-//     spawnObstacle(){
-//         let rX = Math.floor(Math.random() * 400);
-//         let rY = 0;
-//         let rWidth = Math.floor(Math.random() * 50) + 10;
-//         let rHeight = Math.floor(Math.random() * 50) + 10;
-//         let newObstacle = new Obstacle(rX, rY, rWidth, rHeight);
-//         this.obstacleArray.push(newObstacle);
-//         newObstacle.moveDownForever();
-//     }
+    spawnObstacle(){
+        let rX = Math.floor(Math.random() * 400);
+        let rY = 0;
+        let rWidth = Math.floor(Math.random() * 50) + 10;
+        let rHeight = Math.floor(Math.random() * 50) + 10;
+        let newObstacle = new Obstacle(rX, rY, rWidth, rHeight);
+        this.obstacleArray.push(newObstacle);
+        newObstacle.moveDownForever();
+    }
 
-//     clearUnusedObstacles(){
-//         this.obstacleArray.forEach((ob, i)=>{
-//             if(ob.y > 400){
-//                 this.obstacleArray.splice(i, 1)
-//             }
-//         })
-//     }
+    clearUnusedObstacles(){
+        this.obstacleArray.forEach((ob, i)=>{
+            if(ob.y > 400){
+                this.obstacleArray.splice(i, 1)
+            }
+        })
+    }
 
 
-//     collisionDetect(futureX, futureY){
-//         let canMove = true;
+    collisionDetect(futureX, futureY){
+        let canMove = true;
 
-//         this.obstacleArray.forEach((obs)=>{
+        this.obstacleArray.forEach((obs)=>{
 
-//             console.log(futureX, futureY, this.theHero.width, this.theHero.height, obs.x, obs.y, obs.width, obs.height)
+            console.log(futureX, futureY, this.theHero.width, this.theHero.height, obs.x, obs.y, obs.width, obs.height)
 
            
-//         if(futureX + this.theHero.width >= obs.x && futureX <= obs.x + obs.width 
-//             && futureY + this.theHero.height >= obs.y && futureY <= obs.y + obs.height){
-//                 canMove = false;
-//              }
-//         })
+        if(futureX + this.theHero.width >= obs.x && futureX <= obs.x + obs.width 
+            && futureY + this.theHero.height >= obs.y && futureY <= obs.y + obs.height){
+                canMove = false;
+             }
+        })
        
-//         return canMove;
-//     }
-// }
-
-
-
-
-
-
+        return canMove;
+    }
+}
 
 
 
@@ -199,7 +195,7 @@ class Obstacle{
 
 }
 
-document.getElementById('start').onclick = startGame;
+document.getElementById('start-button').onclick = startGame;
 
 
 let theGame;
